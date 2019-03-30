17aeb7002579c9df54b9f2dee86b8fea8eb1fa5a
android_zxh <1786619259@qq.com>
2019-03-30 16:48:03 +0800

    add http

--- app/src/main/java/client/com/basecomponentwithkotlin/App.kt
+++ app/src/main/java/client/com/basecomponentwithkotlin/App.kt
@@ -1,6 +1,21 @@
 package client.com.basecomponentwithkotlin
 
+import android.content.Context
 import client.com.baselib.app.BaseApp
-
+/**
+ * 自定义application
+ *
+ * */
 class App : BaseApp() {
+    override fun onCreate() {
+        super.onCreate()
+        context=this@App
+    }
+    companion object {
+        var context:Context?=null
+        fun getAppContext():Context?{
+            return context
+        }
+
+    }
 }
